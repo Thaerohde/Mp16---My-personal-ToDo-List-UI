41e8f6d75468498cd86d232dfdaa5044e2ae9fc1
Thaer Ohde <thaerohde@Thaers-MacBook-Pro.local>
2018-04-18 15:48:39 +0200

    add spring

---
+++ mp16_my_personal_toDo_list_UI/src/main/java/at/RefugeesCode/mp16_my_personal_toDo_list_UI/Mp16MyPersonalToDoListUiApplication.java
@@ -0,0 +1,12 @@
+package at.RefugeesCode.mp16_my_personal_toDo_list_UI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mp16MyPersonalToDoListUiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mp16MyPersonalToDoListUiApplication.class, args);
+	}
+}
